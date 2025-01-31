2ed83df33a294076597f0bd51689c33c537d70b9
Guifaria12 <gui.faria69@gmail.com>
2025-01-30 22:09:32 -0300

    Update FIAP_Datathon.app.py

--- FIAP_Datathon.app.py
+++ FIAP_Datathon.app.py
@@ -1,1 +1,6 @@
+st.subheader ("""
+**Faculdade de Informática e Administração Paulista – FIAP**
 
+**Grupo 47: Cristiano de Araujo Santos Filho, Eduardo Vilela Silva, Guilherme de Faria
+Rodrigues, Marcelo Pereira Varesi e Vitória Pinheiro de Mattos**
+""")
