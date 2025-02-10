267f66d298c7e9ff16eafa1064f88a8591aa66ea
Guifaria12 <gui.faria69@gmail.com>
2025-02-10 01:04:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,7 +2,6 @@
 import streamlit as st 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from utils import OneHotEncoder, OrdinalEncoder, MinMaxScaler
 from sklearn.pipeline import Pipeline
 import joblib
 from joblib import load
