8c0b3f619f993984148bda38c89d7785d608b8a8
Guifaria12 <gui.faria69@gmail.com>
2025-02-10 00:19:37 -0300

    Update FIAP_Datathon.app.py

--- FIAP_Datathon.app.py
+++ FIAP_Datathon.app.py
@@ -12,3 +12,15 @@ st.header("**Introdução**")
 st.write("""
 A DEFINIR
 """)
+
+url_2022 = 'https://docs.google.com/spreadsheets/d/1td91KoeSgXrUrCVOUkLmONG9Go3LVcXpcNEw_XrL2R0/export?format=csv&gid=90992733'
+url_2023 = 'https://docs.google.com/spreadsheets/d/1td91KoeSgXrUrCVOUkLmONG9Go3LVcXpcNEw_XrL2R0/export?format=csv&gid=555005642'
+url_2024 = 'https://docs.google.com/spreadsheets/d/1td91KoeSgXrUrCVOUkLmONG9Go3LVcXpcNEw_XrL2R0/export?format=csv&gid=215885893'
+
+base_dados_2022 = pd.read_csv(url_2022)
+base_dados_2023 = pd.read_csv(url_2023)
+base_dados_2024 = pd.read_csv(url_2024)
+
+base_dados_2022['origem'] = 'Base2022'
+base_dados_2023['origem'] = 'Base2023'
+base_dados_2024['origem'] = 'Base2024'
