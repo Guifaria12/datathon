b34d114aa2c0fb02e6e3bf9d67c5defa1065688a
Guifaria12 <gui.faria69@gmail.com>
2025-02-10 00:18:58 -0300

    Update FIAP_Datathon.app.py

--- FIAP_Datathon.app.py
+++ FIAP_Datathon.app.py
@@ -10,5 +10,5 @@ Rodrigues, Marcelo Pereira Varesi e Vitória Pinheiro de Mattos**
 st.header("**Introdução**")
 
 st.write("""
-    O preço do petróleo desempenha um papel fundamental na economia global, influenciando mercados financeiros, políticas energéticas e estratégias corporativas em diversos setores, até mesmo em aspectos micro da sociedade (i.e. o preço da gasolina no posto que cotidianamente se abastece o carro para ir ao trabalho). Por ser um dos principais indicadores econômicos, sociais e políticos, compreender sua dinâmica de variação ao longo do tempo é essencial para a tomada de decisões estratégicas em um ambiente econômico volátil e interconectado.
+A DEFINIR
 """)
