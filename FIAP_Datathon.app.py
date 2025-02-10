0b6e42df451b190c694b41077d24fe3f48d83a6d
Guifaria12 <gui.faria69@gmail.com>
2025-02-10 00:10:10 -0300

    Update FIAP_Datathon.app.py

--- FIAP_Datathon.app.py
+++ FIAP_Datathon.app.py
@@ -24,6 +24,18 @@ base_completa.loc[(base_completa['Ano ingresso'] != 2024) & (base_completa['orig
 
 base_completa['Status_entrada'].fillna('Desistente', inplace=True)
 
+st.subheader ("""
+**Faculdade de Informática e Administração Paulista – FIAP**
+
+**Grupo 47: Cristiano de Araujo Santos Filho, Eduardo Vilela Silva, Guilherme de Faria
+Rodrigues, Marcelo Pereira Varesi e Vitória Pinheiro de Mattos**
+""")
+st.header("**Introdução**")
+
+st.write("""
+    O preço do petróleo desempenha um papel fundamental na economia global, influenciando mercados financeiros, políticas energéticas e estratégias corporativas em diversos setores, até mesmo em aspectos micro da sociedade (i.e. o preço da gasolina no posto que cotidianamente se abastece o carro para ir ao trabalho). Por ser um dos principais indicadores econômicos, sociais e políticos, compreender sua dinâmica de variação ao longo do tempo é essencial para a tomada de decisões estratégicas em um ambiente econômico volátil e interconectado.
+""")
+
 import numpy as np
 
 base_completa.replace('#DIV/0!', np.nan, inplace=True)
